8fe35d124dbc51b042fddc78ba5b90ae91cb9e7a
dorime <aswalrishika09@gmail.com>
2025-11-20 09:22:38 +0530

    Add program to calculate circle circumference

---
+++ day2.4.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	float r,pie,circumference;
+	printf("enter the value of radius");
+	scanf("%f",&r);
+	pie= 3.14;
+	circumference=2*pie*r;
+	printf("the circumference of the circle is=%f",circumference);
+	
+}
