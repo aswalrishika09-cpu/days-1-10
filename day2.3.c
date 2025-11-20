f6f92579836b950aaefec46e76ca59842369867d
dorime <aswalrishika09@gmail.com>
2025-11-20 09:21:56 +0530

    Add perimeter calculation for rectangle

---
+++ day2.3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int length,breath, peri;
+	printf("enter a length");
+	scanf("%d",&length);
+	printf("enter breath");
+	scanf("%d",&breath);
+	peri= 2*(length+breath);
+	printf("perimeter =%d",peri);
+}
